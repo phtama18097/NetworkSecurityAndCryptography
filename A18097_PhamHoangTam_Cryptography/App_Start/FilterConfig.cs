44f06b80c03d4758cd8ae205215d30d86dd4d286
Admin <Admin@DESKTOP-M21QKUC>
2021-05-31 11:41:16 +0700

    Add project files.

---
+++ A18097_PhamHoangTam_Cryptography/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace A18097_PhamHoangTam_Cryptography
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
